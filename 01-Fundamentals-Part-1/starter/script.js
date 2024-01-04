a66cc58b6b72709c77a68c4c6d0a367a2d41cf73
yangwanyun <ysrdywywork@163.com>
2024-01-04 16:55:38 +0800

    c 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,34 @@
+/*
 let js = 'exciting';
-if (js === 'exciting') alert("js is fun");
-
 console.log(40 + 30);
+
+console.log('yang');
+console.log(22);
+
+// variable name
+let firstName = 'li';
+console.log(firstName);
+
+let PI = 3.14;
+*/
+
+/*
+let jsIsFun = true;
+console.log(jsIsFun);
+console.log(typeof true);
+console.log(typeof jsIsFun);
+console.log(typeof 22);
+console.log(typeof 'sss');
+
+jsIsFun = "yes";
+console.log(typeof jsIsFun);
+
+let year;
+console.log(typeof year);   // undefined
+
+year = 2024;
+console.log(year, typeof year);
+
+console.log(typeof null);   // object
+*/
+
