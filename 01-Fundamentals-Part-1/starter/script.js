44de9df64b1c75ceb9125d50ada8e13ce592163f
yangwanyun <ysrdywywork@163.com>
2024-01-05 09:47:41 +0800

    coding challenge #1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -32,3 +32,64 @@ console.log(year, typeof year);
 console.log(typeof null);   // object
 */
 
+/*
+let age = 20;
+age = 21;
+
+const birthYear = 2000;   // const 声明的变量不可修改
+// const 声明变量时必须有初始值
+
+const now = 2039;
+const ageJonas = now - birthYear;
+console.log(ageJonas);
+console.log(4 * 2, 30 / 10, 2 ** 3)
+
+const firstName = 'yang';
+const lastName = 'wy';
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+let markWeights = 78;
+let markHeight = 1.69;
+let johnWeights = 92;
+let johnHeight = 1.95;
+
+let markBmi = markWeights / markHeight ** 2;
+let johnBmi = johnWeights / johnHeight ** 2;
+let markHigherBMI = markBmi > johnBmi;
+if (markHigherBMI === true) {
+  console.log("Mark has a higher BMI than John");
+} else {
+  console.log("Mark has a lower BMI than John");
+}
+
+// test data 2
+markWeights = 95;
+markHeight = 1.88;
+johnWeights = 85;
+johnHeight = 1.76;
+
+markBmi = markWeights / markHeight ** 2;
+johnBmi = johnWeights / johnHeight ** 2;
+markHigherBMI = markBmi > johnBmi;
+if (markHigherBMI === true) {
+  console.log("Mark has a higher BMI than John");
+} else {
+  console.log("Mark has a lower BMI than John");
+}
+
+// teacher version
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIJohn, BMIMark, markHigherBMI);
+
